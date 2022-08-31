ae3c3ffc9b09486c891aecf19ea5ee2b0cd44de8
Ayoub LAOUAD (contractor) <ayoub.laouad@amadeus.com>
2022-08-31 19:18:08 +0200

    add-second-page-and-render-body-and-DbContext

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,40 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Owner>().Property(x => x.Id).HasDefaultValue("NEWID");
+            modelBuilder.Entity<PortFolioItem>().Property(x => x.Id).HasDefaultValue("NEWID");
+
+
+
+            modelBuilder.Entity<Owner>().HasData
+            (
+                new Owner()
+                {
+                    Id = Guid.NewGuid(),
+                    _Avatar = "pdp.jfif",
+                    _FullName = "Ayoub LAOUAD",
+                    _Profil = "Backend Developer"
+                }
+
+            );
+        }
+
+        public DbSet<Owner> Owner { get; set; }S
+        public DbSet<PortFolioItem> PortFolioItems { get; set; }
+
+    }
+}
