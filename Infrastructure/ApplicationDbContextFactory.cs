c6ff0606b9ef8cf0f99a5bda2d7d47ca83b21c4e
LaouadAyoub <ayoubel1998@gmail.com>
2022-10-10 02:21:06 +0200

    more-change-connection-string

--- Infrastructure/ApplicationDbContextFactory.cs
+++ Infrastructure/ApplicationDbContextFactory.cs
@@ -8,7 +8,7 @@ namespace Infrastructure
         public DataContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
-            optionsBuilder.UseSqlServer("Host = ec2-54-75-26-218.eu-west-1.compute.amazonaws.com; Port = 5432; user Id= mzpzuxcyrrnfer; Password = f907f8937231868374feb238c4eed88bd569bc08a16f6c56a5510c16199a2c4b; Database = dbca8euvjcqje3; URI = postgres://mzpzuxcyrrnfer:f907f8937231868374feb238c4eed88bd569bc08a16f6c56a5510c16199a2c4b@ec2-54-75-26-218.eu-west-1.compute.amazonaws.com:5432/dbca8euvjcqje3; Heroku CLI = heroku pg:psql postgresql-shaped-90811 --app portfolioayoub ");
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial catalog=MyPortfolioDB; Integrated Security=true;");
 
             return new DataContext(optionsBuilder.Options);
         }
